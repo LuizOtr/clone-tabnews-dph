22fefdd145ac6dbf2e4ecb0d3a05ae3384ec6078
Luiz Cordeiro <65364150+LuizOtr@users.noreply.github.com>
2025-12-03 23:36:04 +0000

    Alterado Frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Salve, blz?</h1>
+    return <h1>Foi quando Jairo finalmente entendeu</h1>
 }
 
 export default Home; // Este é o ponto de partida (índice) usado para construir os próximos elementos.
